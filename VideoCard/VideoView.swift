3179eba6644b9744aec6c2a63e00e8754ce3f25d
Jean-Marc Boullianne <jboullianne@gmail.com>
2019-12-21 23:29:57 -0500

    Created UIViewRepresentableClass utilizing PlayerView

--- VideoCard/VideoView.swift
+++ VideoCard/VideoView.swift
@@ -6,4 +6,29 @@
 //  Copyright © 2019 TrailingClosure. All rights reserved.
 //
 
-import Foundation
+import SwiftUI
+import AVFoundation
+import AVKit
+
+struct VideoView: UIViewRepresentable {
+    
+    var videoURL:URL
+    var previewLength:Double
+    
+    func makeUIView(context: Context) -> UIView {
+        return PlayerView(frame: .zero, url: videoURL, previewLength: previewLength)
+    }
+    
+    func updateUIView(_ uiView: UIView, context: Context) {
+        
+    }
+}
+
+struct VideoView_Previews: PreviewProvider {
+    
+    static var url:URL = URL(string: "https://bitdash-a.akamaihd.net/content/sintel/hls/playlist.m3u8")!
+    
+    static var previews: some View {
+        VideoView(videoURL: url, previewLength: 15)
+    }
+}
