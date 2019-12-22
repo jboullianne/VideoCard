34681f9b241f72f32fa4b6661594f77a15d47823
Jean-Marc Boullianne <jboullianne@gmail.com>
2019-12-21 23:30:57 -0500

    Added example to ContentView for VideoCard.

--- VideoCard/ContentView.swift
+++ VideoCard/ContentView.swift
@@ -9,8 +9,19 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var maxHeight:CGFloat = 200
+    
     var body: some View {
-        Text("Hello, World!")
+        VStack{
+            VideoView(videoURL: URL(string: "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/TearsOfSteel.mp4")!, previewLength: 60)
+                .cornerRadius(15)
+                .frame(width: nil, height: maxHeight, alignment: .center)
+                .shadow(color: Color.black.opacity(0.7), radius: 30, x: 0, y: 2)
+                .padding(.horizontal, 20)
+                .padding(.top, 20)
+
+            Spacer()
+        }
     }
 }
 
