14edac3b4069ebc9d1bcab122be05e2bdbd6fd34
Jean-Marc Boullianne <jboullianne@gmail.com>
2019-12-21 23:30:26 -0500

    Created upgraded VideoView which shows a play icon and support tap gestures.

--- VideoCard/VideoCard.swift
+++ VideoCard/VideoCard.swift
@@ -9,13 +9,34 @@
 import SwiftUI
 
 struct VideoCard: View {
+    
+    @State var videoURL:URL
+    @State var showPlayIcon:Bool
+    var previewLength:Double
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            VideoView(videoURL: videoURL, previewLength: previewLength)
+            if showPlayIcon {
+                Image(systemName: "play.circle.fill")
+                .resizable()
+                .scaledToFit()
+                .frame(minWidth: 20, idealWidth: 40, maxWidth: 40, minHeight: 20, idealHeight: 40, maxHeight: 40, alignment: .center)
+                .foregroundColor(Color.white)
+            }
+            
+        }
+        .onTapGesture {
+            print("DID TAP VIDEO")
+        }
     }
 }
 
 struct VideoCard_Previews: PreviewProvider {
+    
+    static let url = URL(string: "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/TearsOfSteel.mp4")!
+    
     static var previews: some View {
-        VideoCard()
+        VideoCard(videoURL: url, showPlayIcon: true, previewLength: 20)
     }
 }
